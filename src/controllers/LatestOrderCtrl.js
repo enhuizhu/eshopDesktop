1ab095c8b8c3ebc173fa29534cd3a8ec5abde10e
enhui zhu <enhuizhu@Dxis-Mac-mini.local>
2016-10-19 18:02:26 +0100

    udpate

--- src/controllers/LatestOrderCtrl.js
+++ src/controllers/LatestOrderCtrl.js
@@ -1,16 +1,17 @@
 "use strict";
 
 angular.module("eshop").controller("LatestOrderCtrl", function() {
-		var controller = function($scope, orders) {
+		var controller = function($scope, orders, timeService) {
 			this.init.apply(this, arguments);
 		}
 
 		controller.prototype = {
-			init: function($scope, orders) {
+			init: function($scope, orders, timeService) {
 				$scope.orders = orders;
+				$scope.timeService = timeService;
 			}
 		}
 
-		controller.$inject = ['$scope', 'orders'];
+		controller.$inject = ['$scope', 'orders', 'timeService'];
 		return controller;
 }());
